6ed720a81b97c1066de3bc9acdcb9fc5089e3ec6
kkucuk3585 <kullikulli85@gmail.com>
2024-04-14 20:53:14 +0300

    Iorderservice updated

--- src/BlazorShared/Interfaces/IOrderService.cs
+++ src/BlazorShared/Interfaces/IOrderService.cs
@@ -6,11 +6,6 @@ namespace BlazorShared.Interfaces;
 
 public interface IOrderService
 {
-    //Task<CatalogItem> Create(CreateCatalogItemRequest catalogItem);
-    //Task<CatalogItem> Edit(CatalogItem catalogItem);
-    //Task<string> Delete(int id);
-    //Task<CatalogItem> GetById(int id);
-    //Task<List<CatalogItem>> ListPaged(int pageSize);
     Task<List<Orders>> GetOrders();
     Task<OrderDetail> GetOrdersById(int id);
     Task<Orders> UpdateOrder(Orders order);
